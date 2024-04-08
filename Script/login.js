860e43446ad67ba86790bf44931fb393aaff4836
rachitpoudel101 <rachit300poudel@gmail.com>
2024-04-08 14:29:14 +0545

    Delete unnessery files

--- Script/login.js
+++
@@ -1,9 +0,0 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var userInfo = document.getElementById('userInfo');
-    userInfo.innerHTML = '<p>ID: ' + profile.getId() + '</p>' +
-                          '<p>Name: ' + profile.getName() + '</p>' +
-                          '<p>Email: ' + profile.getEmail() + '</p>' +
-                          '<img src="' + profile.getImageUrl() + '" alt="Profile Picture">';
-  }
-  
