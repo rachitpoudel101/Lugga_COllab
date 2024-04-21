d3420d867b7529bc5628d246e201f4d6d9048aa5
prajal <143073769+Prajalshaha@users.noreply.github.com>
2024-04-21 10:01:46 +0545

    Add comment header.php

--- admin/header.php
+++ admin/header.php
@@ -40,13 +40,16 @@ Custom styles for this template -> <link href="dashboard.css" rel="stylesheet">
 -->
 
 <body>
+    <!-- Header section -->
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-Ig-2 me-0 px-3" href="#">Company name</a>
+        <!-- Navbar toggler button -->
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-nav">
             <div class="nav-item text-nowrap">
+                <!-- Sign out link -->
                 <a class="nav-link px-3" href="logout.php?logout=1">Sign out</a>
             </div>
         </div>
