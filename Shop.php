09593ccc9070f308a5c9f70a398d4c99fcb45650
Swarup Devkota <120736413+SwarupDevkota@users.noreply.github.com>
2024-04-21 08:42:27 +0545

    Update Shop.php

--- Shop.php
+++ Shop.php
@@ -1,244 +1,127 @@
 <?php
-session_start();
-include("server/connection.php");
+include('server/connection.php');
 
-if (isset($_SESSION['logged_in'])) {
-    header("location: account.php");
-    exit;
-}
-
-if (isset($_POST['login_btn'])) {
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
-
-    $stmt = $conn->prepare("SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email = ? AND user_password = ?");
-    $stmt->bind_param('ss', $email, $password);
-
-    if ($stmt->execute()) {
-        $stmt->bind_result($user_id, $username, $user_email, $user_password);
-
-        if ($stmt->fetch()) {
-            $_SESSION['user_id'] = $user_id;
-            $_SESSION['user_name'] = $username;
-            $_SESSION['user_email'] = $user_email;
-            $_SESSION['logged_in'] = true;
-            header('location: account.php?message=logged_in_successfully');
-        } else {
-            header('location: login.php?error=could_not_verify_your_account');
-        }
+// Use the search section
+if (isset($_POST['search'])) {
+    // 1. Determine page no
+    if (isset($_GET['page_no']) && $_GET['page_no'] != '') {
+        // If user has already entered page then page number is the one that they selected
+        $page_no = $_GET['page_no'];
     } else {
-        // Error: Something went wrong with the execution of the query
-        header('location: login.php?error=something_went_wrong');
+        // If user just entered the page then default page is 1
+        $page_no = 1;
     }
+
+    $category = $_POST['category'];
+    $price = $_POST['price'];
+
+    // 2. Return number of products
+    $stmt1 = $conn->prepare("SELECT COUNT(*) As total_records FROM products WHERE product_category=? AND product_price<=?");
+    $stmt1->bind_param('si', $category, $price);
+    $stmt1->execute();
+    $stmt1->bind_result($total_records);
+    $stmt1->store_result();
+    $stmt1->fetch();
+
+    // 3. Products per page
+    $total_records_per_page = 8;
+    $offset = ($page_no - 1) * $total_records_per_page;
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
+    $adjacents = 2;
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+
+    // 4. Get all products
+    $stmt2 = $conn->prepare("SELECT * FROM products WHERE product_category=? AND product_price<=? LIMIT $offset, $total_records_per_page");
+    $stmt2->bind_param('si', $category, $price);
+    $stmt2->execute();
+    $products = $stmt2->get_result();
+} else {
+    // Determine page number
+    if (isset($_GET['page_no']) && $_GET['page_no'] != '') {
+        // If user has already entered page then page number is the one that they selected
+        $page_no = $_GET['page_no'];
+    } else {
+        // If user just entered the page then default page is 1
+        $page_no = 1;
+    }
+
+    // Return number of products
+    $stmt1 = $conn->prepare("SELECT COUNT(*) AS total_records FROM products");
+    $stmt1->execute();
+    $stmt1->bind_result($total_records);
+    $stmt1->store_result();
+    $stmt1->fetch();
+
+    // Products per page
+    $total_records_per_page = 8;
+    $offset = ($page_no - 1) * $total_records_per_page;
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
+    $adjacents = 2;
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+
+    // Get all products
+    $stmt2 = $conn->prepare("SELECT * FROM products LIMIT ?, ?");
+    $stmt2->bind_param('ii', $offset, $total_records_per_page);
+    $stmt2->execute();
+    $products = $stmt2->get_result();
 }
 ?>
 
+<?php include('layouts/header.php'); ?>
 
 
 
+<!-- Search Section -->
+<section id="search" class="my-5 py-5 ms-2">
+    <div class="container mt-5 py-5">
+        <p>Search Products</p>
+    </div>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/assets/css/index.css">
-</head>
-
-<body>
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 fixed top">
-        <div class="container">
-            <img src="/assets/Imgs/logo.png" alt="Logo">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/Html/index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Html/Shop.html">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-disabled="true">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-disabled="true">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <i class="fas fa-shopping-bag"></i>
-                        <i class="fa-solid fa-user"></i>
-                    </li>
-                </ul>
+    <form action="shop.php" method="POST">
+        <div class="row mx-auto container">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <p>Category</p>
+                <div class="form-check">
+                    <input class="form-check-input" value="shoes" type="radio" name="category" id="category_one" <?php if (isset($category) && $category == 'shoes') {
+                                                                                                                        echo 'checked';
+                                                                                                                    } ?>>
+                    <label class="form-check-label" for="category_one">Shoes</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" value="coats" type="radio" name="category" id="category_two" <?php if (isset($category) && $category == 'Coats') {
+                                                                                                                        echo 'checked';
+                                                                                                                    } ?>>
+                    <label class="form-check-label" for="category_two">Coats</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" value="watches" type="radio" name="category" id="category_three" <?php if (isset($category) && $category == 'Watches') {
+                                                                                                                            echo 'checked';
+                                                                                                                        } ?>>
+                    <label class="form-check-label" for="category_three">Watches</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" value="bags" type="radio" name="category" id="category_four" <?php if (isset($category) && $category == 'Bags') {
+                                                                                                                        echo 'checked';
+                                                                                                                    } ?>>
+                    <label class="form-check-label" for="category_four">Bags</label>
+                </div>
             </div>
         </div>
-    </nav>
 
-    <!-- Login -->
-    <section class="my-5 py-5">
-        <div class="container text-center mt-3 pt-5">
-            <h2 class="font-weight-bold">Login</h2>
-            <hr class="mx-auto">
+        <div class="row mx-auto container mt-5">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <p>Price</p>
+                <div class="w-50">
+                    <span style="float: left;">1</span> <span style="float: right;">1000</span>
+                </div>
+            </div>
         </div>
-        <div class="mx-auto container" method="POST" action="login.php">
-            <p style="color:red" class="text-center"><?php if (isset($_GET['error'])) echo htmlspecialchars($_GET['error']); ?></p>
+        <input type="range" class="form-range w-50" name="price" value="<?php echo isset($price) ? $price : ''; ?>" min="0" max="1000">
 
-            <form id="login-form">
-                <div class="form-group">
-                    <label for="login-email">Email</label>
-                    <input type="text" class="form-control" id="login-email" name="email" placeholder="Email" required>
-                </div>
-                <div class="form-group">
-                    <label for="login-password">Password</label>
-                    <input type="password" class="form-control" id="login-password" name="password" placeholder="Password" required>
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btn" id="login-btn" name="login_btn value=" Login">
-                </div>
-                <div class="form-group">
-                    <a id="register-url" class="btn">Don't have an account? Register</a>
-                </div>
-            </form>
+        <div class="form-group my-3 mx-3">
+            <input type="submit" name="search" value="Search" class="btn btn-primary">
         </div>
-    </section>
-
-
-
-
-
-
-    <!--Footer-->
-    <footer class="bg-body-tertiary text-center">
-        <div class="container p-4">
-            <section class="mb-4">
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
-            </section>
-            <section class="">
-                <form action="">
-                    <div class="row d-flex justify-content-center">
-                        <div class="col-auto">
-                            <p class="pt-2">
-                                <strong>Sign up for our Website</strong>
-                            </p>
-                        </div>
-                        <div class="col-md-5 col-12">
-                            <div data-mdb-input-init class="form-outline mb-4">
-                                <input type="email" id="form5Example24" class="form-control" />
-                                <label class="form-label" for="form5Example24">Email address</label>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <button data-mdb-ripple-init type="submit" class="btn btn-outline mb-4">
-                                Subscribe
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </section>
-
-            <section class="mb-4">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-                    repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum
-                    harum corrupti dicta, aliquam sequi voluptate quas.
-                </p>
-            </section>
-            <section class="">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">Contact Us </h5>
-
-                        <ul class="list-unstyled mb-0">
-                            <li>
-                                <a class="text-body" href="#!">Shop</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">account</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">About Us</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">Contact Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">Cataogery</h5>
-
-                        <ul class="list-unstyled mb-0">
-                            <li>
-                                <a class="text-body" href="#!">Men</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">Woman</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">kids</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!"></a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">Seasonal</h5>
-
-                        <ul class="list-unstyled mb-0">
-                            <li>
-                                <a class="text-body" href="#!">Winter</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">Spring</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">Autum</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">sUMMER</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5 class="text-uppercase">account</h5>
-
-                        <ul class="list-unstyled mb-0">
-                            <li>
-                                <a class="text-body" href="#!">My account</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">Cart</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">paymethod</a>
-                            </li>
-                            <li>
-                                <a class="text-body" href="#!">My dashboard</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-        </div>
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-            © 2020 Copyright:
-            <a class="text-reset fw-bold" href="">Lugga</a>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    </form>
+</section>
