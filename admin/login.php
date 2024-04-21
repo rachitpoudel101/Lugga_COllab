279a18c4acf2674c2a0b500cac5b066a096d8c7f
UtsavTuladhar <165638396+UtsavTuladhar@users.noreply.github.com>
2024-04-21 09:47:30 +0545

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -44,3 +44,39 @@ if (isset($_POST['login_btn'])) {
     exit;
 }
 ?>
+<?php include( 'header.php'); ?>
+<div class="container-fluid">
+    <div class="" style="min-height: 1000px">
+        <main class="col-md-6 mx-auto col-lg-6 px-md-4 text-center">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-item">
+                <h1 class="h2">c/h1> <!-- This line should be corrected -->
+                <div class="btn-toolbar mb-2 mb-md-g">
+                    <div class="btn-group me-2">
+                        <!-- Buttons -->
+                    </div>
+                </div>
+            </div>
+            <h2>Login</h2>
+            <div class="table-responsive">
+                <div class="mx-auto container">
+                    <form id="login-form" enctype="multipart/form-data" method="POST" action="">
+                        <p style="color: red;"><?php if (isset($_GET['error'])) {
+                                                    echo $_GET['error'];
+                                                } ?></p>
+                        <div class="form-group mt-2">
+                            <label>Email</label>
+                            <input type="email" class="form-control" id="product-name" name="email">
+                        </div>
+                        <div class="form-group mt-2">
+                            <label>Password</label>
+                            <input type="password" class="form-control" id="product-desc" name="password">
+                        </div>
+                        <div class="form-group mt-3">
+                            <input type="submit" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
