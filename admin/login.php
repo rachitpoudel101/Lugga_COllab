45ae2b52b159c1aaba611203cc25cf22340813cb
UtsavTuladhar <165638396+UtsavTuladhar@users.noreply.github.com>
2024-04-21 09:45:54 +0545

    create admin login.php

--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
+include('server/connection.php');
+
+if (isset($_SESSION['admin_logged_in'])) {
+    header('location: index.php');
+    exit;
+}
+
+if (isset($_POST['login_btn'])) {
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
 
